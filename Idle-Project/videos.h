7e46e3f68588efbdfa3b25b1264579382a618dcb
JAbinSin <johnalvincruz93@gmail.com>
2022-06-14 20:42:53 +0800

    Added Comments

--- Idle-Project/videos.h
+++ Idle-Project/videos.h
@@ -6,29 +6,30 @@ namespace ns
 {
 	class VideoList	{
 	   private:
-		   struct VideoNode {
+			// This Structure would be a Linked List
+			struct VideoNode {
 			    int videoId;
-                std::string videoTitle;
+            	std::string videoTitle;
                 std::string videoGenre;
 				std::string videoProduction;
 				int videoCopies;
 				std::string videoImage;
 			    struct VideoNode *next;
-		   };
-		   VideoNode *head, *current;
+		   	};
+		   	VideoNode *head, *current;
         public:
-			// The Codes below are a public Function
+			// The Codes below are the public Function
 	        VideoList(); // Constructor
 		    ~VideoList(); // Deconstructor
 		    void insertVideo(int id, std::string title, std::string genre, std::string production, int copies, std::string image); // Insertion of a New Video
 		    void rentVideo(int id); // Renting a Video
 		    void returnVideo(int id); // Return all the video that the Customer rent/s
 		    void videoDetails(int id); // Details about the specified Video using Video ID
-			void videoRentDetails(int id);
+			void videoRentDetails(int id); // Would output the Movie Title and Number of Copies, of the specific user
             void videoList(); // List all the Video and their Details
-			void videoAvailableDetail(int id); // Check if the Video is Available by looking at the Numebr of Copies
+			void videoAvailableDetail(int id); // Check if the Video is Available by looking at the Number of Copies
 			int autoVideoIdIncrement(); // For the Auto Generation of Video ID    
-			bool videoAvailable(int id);
-			std::string videoTitle(int id);
+			bool videoAvailable(int id); // Would return true if the video has 1 or more number of copies
+			std::string videoTitle(int id); // Would return the title of the specific video
 	};
 }
